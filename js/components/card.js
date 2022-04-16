2711a0410382b21a0584808cd05c8bea7284fda7
Mahiny <93958141+Mahiny@users.noreply.github.com>
2022-04-16 16:49:27 -0300

    atualizando

--- js/components/card.js
+++ js/components/card.js
@@ -1,12 +1,12 @@
 class CardComponent extends HTMLElement {
 
     cards = [
-        {src: './../assets/img/1917.png', title: '1917', description: 'Aooba'},
-        {src: './../assets/img/after.png', title: 'after', description: 'Aooba'},
-        {src: './../assets/img/dora.png', title: 'dora', description: 'Aooba'},
-        {src: './../assets/img/capita-marvel.png', title: 'capitã marvel', description: 'Aooba'},
-        {src: './../assets/img/joker.png', title: 'joker', description: 'Aooba'},
-        {src: './../assets/img/thor.png', title: 'thor', description: 'Aooba'},
+        {src: './../assets/img/1917.png', title: '1917', description: '1917 conta a história de dois soldados britânicos em um dos momentos mais críticos da Primeira Guerra Mundial. Em uma dramática corrida contra o tempo, os soldados deverão cruzar o território inimigo e entregar uma mensagem que cessará o ataque brutal a milhares.'},
+        {src: './../assets/img/after.png', title: 'after', description: 'After é um filme americano de romance baseado no best-seller e livro homônimo da autora Anna Todd, com direção de Jenny Gage e roteiro de Susan McMartin. O filme é estrelado por Josephine Langford como Tessa Young.'},
+        {src: './../assets/img/dora.png', title: 'dora', description: 'Ambientado na floresta peruana, o filme narra as aventuras de Dora (Isabella Merced) junto de seu macaco Botas, amigos que acabou de fazer na escola e um misterioso explorador a fim de salvar seus pais de mercenários.'},
+        {src: './../assets/img/capita-marvel.png', title: 'capitã marvel', description: 'Em Capitã Marvel, Carol Danvers (Brie Larson) é uma ex-agente da Força Aérea norte-americana, que, sem se lembrar de sua vida na Terra, é recrutada pelos Kree para fazer parte de seu exército de elite.'},
+        {src: './../assets/img/joker.png', title: 'joker', description: 'Baseado no personagem de mesmo nome da DC Comics, o filme é estrelado por Joaquin Phoenix como o Coringa. Joker é ambientado em 1981, e representa Arthur Fleck, um comediante de stand-up fracassado, que é levado à loucura e se envolve em uma vida de crimes e caos em Gotham City.'},
+        {src: './../assets/img/thor.png', title: 'thor', description: 'Thor (Chris Hemsworth) estava prestes a receber o comando de Asgard das mãos de seu pai Odin (Anthony Hopkins) quando forças inimigas quebraram um acordo de paz.'},
     ];
 
     constructor() {
@@ -59,7 +59,7 @@ class CardComponent extends HTMLElement {
             }
             
             .card:hover .card__info {
-                height: 90%;
+                height: 60%;
                 background-color: rgba(244, 244, 244, .9);
             }
             
