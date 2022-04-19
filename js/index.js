661560ca866c9fe8457b77d80430961810b0167c
Gabriel Henrique <gabriel.ghvn@gmail.com>
2022-04-18 23:40:36 -0300

    menu mobile css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
     
 async function buscarFilme(filme, trailer) {
-    await fetch(`https://imdb-api.com/pt-BR/API/Title/k_lnmf1a2b/${filme}`)
+    await fetch(`https://imdb-api.com/pt-br/API/Title/k_lnmf1a2b/${filme}`)
     .then(response => response.text())
     .then(result => localStorage.setItem('filme', result))
     .catch(error => console.log('error', error));
