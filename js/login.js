fadf11ff439b0652e3c1500e4ca1e35205fc4c0b
Gabriel Henrique <gabriel.ghvn@gmail.com>
2022-04-20 23:00:14 -0300

    :fix: modal click event fixed

--- js/login.js
+++ js/login.js
@@ -3,6 +3,12 @@ backdrop.addEventListener('click', () => {
     let login = document.querySelector('.backdrop');
     login.classList.add('displayNone');
 })
+
+let modalContainer = document.querySelector('.modal-container')
+modalContainer.addEventListener('click', (event) => {
+    event.stopPropagation();
+})
+
 backdrop.addEventListener('keydown ', (event) => {
     if(event.which ==  27) {
         let login = document.querySelector('.backdrop');
